874cc70cf2322bd84e6ae74f8517e5cb80bd823e
Manel Daza (ANEK) <127338918+ManelDaza@users.noreply.github.com>
2024-10-16 21:24:42 +0200

    Initial commit

---
+++ src/JavaClassesTutorial.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author Manel
+ */
+import java.util.Scanner;
+public class JavaClassesTutorial {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String name = " ";
+        
+        System.out.println("HOLA, Bienvenido al mundo de los Pacomones! (nintendo no me demandes)");
+        System.out.println("Antes de comenzar tu aventura dime, como te llamas?");
+        name = scan.nextLine();
+        System.out.println("Encantado de conocerte " + name + " Espero que estes listo para esta corta aventura!");
+        
+        System.out.println("Ahora a llegado el momento al que has estado esperando, ESCOJE TU PACOMON!");
+        
+    }
+    
+}
